36c4599f479035a8199b885314c07b3f7996e9f5
Damien Cuda <99328663+DamienCuda@users.noreply.github.com>
2024-02-12 15:23:27 +0100

    latest update

---
+++ src/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('updatedAt')
+            ->add('slug')
+            ->add('isActu')
+            ->add('image')
+            ->add('meta')
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
