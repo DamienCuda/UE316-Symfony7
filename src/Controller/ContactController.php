9846fea7c2c67b6aa80ef37de83e5be70ab7b23f
Nils <nils.gill.arion@gmail.com>
2024-02-15 16:37:32 +0100

    Formulaire fonctionnel

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mime\HtmlToTextConverter\DefaultHtmlToTextConverter;
+
 
 #[Route('/contact')]
 class ContactController extends AbstractController
 {   
-    #[Route('/', name: 'app_contact_index', methods: ['GET'])]
+    #[Route('/', name: 'app_contact_index', methods: ['GET', 'POST'])]
     public function contact(Request $request, MailerInterface $mailer)
     {
         $contact = new Contact();
@@ -22,16 +26,18 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
             $message = (new Email())
-                ->setFrom($contact->getEmail())
-                ->setTo('nils.gill.arion@gmail.com')
-                ->setBody($contact->getMessage());
+                ->from($contact->getEmail())
+                ->to('contact@localhost.fr')
+                ->subject('Nouveau message de contact')
+                ->text($contact->getMessage());
 
             $mailer->send($message);
 
             $this->addFlash('success', '');
 
-            return $this->redirectToRoute('app_contact_index');
+            return $this->redirectToRoute('app_contact_index', [], Response::HTTP_SEE_OTHER);
         }
 
 
