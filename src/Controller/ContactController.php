1168b31ee21e869e53045360c0ad729803e7493e
Nils <nils.gill.arion@gmail.com>
2024-02-15 14:28:49 +0100

    Page de contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/contact')]
+class ContactController extends AbstractController
+{   
+    #[Route('/', name: 'app_contact_index', methods: ['GET'])]
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = (new Email())
+                ->setFrom($contact->getEmail())
+                ->setTo('nils.gill.arion@gmail.com')
+                ->setBody($contact->getMessage());
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
+
+            return $this->redirectToRoute('app_contact_index');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
