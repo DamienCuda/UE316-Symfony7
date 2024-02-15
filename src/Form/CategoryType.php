b61b5ed54c0f0004a7a5149c4ae59126e19155c6
Nils <nils.gill.arion@gmail.com>
2024-02-15 15:00:49 +0100

    Debug suite au merge

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Post;
-use App\Entity\Training;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
