0b541a9afda1d9c3cd143713da6581bf58725866
Nils <nils.gill.arion@gmail.com>
2024-02-16 14:48:22 +0100

    Pagination
    Utilisation d'une nouvelle dépendance, knp paginator

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,15 +10,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/post')]
 class PostController extends AbstractController
 {
     #[Route('/', name: 'app_post_index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, Request $request, PaginatorInterface  $paginator): Response
     {
+        $post = $postRepository->findAll();
+
+        $pagination = $paginator->paginate(
+            $post,
+            $request->query->getInt('page', 1),
+            6
+    );
+
         return $this->render('post/index.html.twig', [
-            'posts' => $postRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
